99305b454993ca8f640dca9953cb44c8a655b104
mayssazaghrate <mayssazaghrate2@gmail.com>
2026-05-06 19:03:19 +0100

    stock

--- src/main/java/com/medical/model/Stock.java
+++ src/main/java/com/medical/model/Stock.java
@@ -31,30 +31,30 @@ public class Stock {
         this.fournisseur    = fournisseur;
     }
 
-    public int    getIdStock()                          { return idStock; }
-    public void   setIdStock(int idStock)               { this.idStock = idStock; }
-    public int    getIdMedicament()                     { return idMedicament; }
-    public void   setIdMedicament(int idMedicament)     { this.idMedicament = idMedicament; }
-    public String getNumeroLot()                        { return numeroLot; }
-    public void   setNumeroLot(String numeroLot)        { this.numeroLot = numeroLot; }
-    public int    getQuantite()                         { return quantite; }
-    public void   setQuantite(int quantite)             { this.quantite = quantite; }
-    public int    getSeuilAlerte()                      { return seuilAlerte; }
-    public void   setSeuilAlerte(int seuilAlerte)       { this.seuilAlerte = seuilAlerte; }
-    public double getPrixUnitaire()                     { return prixUnitaire; }
-    public void   setPrixUnitaire(double prixUnitaire)  { this.prixUnitaire = prixUnitaire; }
-    public Date   getDateExpiration()                   { return dateExpiration; }
-    public void   setDateExpiration(Date dateExpiration){ this.dateExpiration = dateExpiration; }
-    public Date   getDateReception()                    { return dateReception; }
-    public void   setDateReception(Date dateReception)  { this.dateReception = dateReception; }
-    public String getLocalisation()                     { return localisation; }
-    public void   setLocalisation(String localisation)  { this.localisation = localisation; }
-    public String getFournisseur()                      { return fournisseur; }
-    public void   setFournisseur(String fournisseur)    { this.fournisseur = fournisseur; }
+    public int    getIdStock()                         { return idStock; }
+    public void   setIdStock(int idStock)              { this.idStock = idStock; }
+    public int    getIdMedicament()                    { return idMedicament; }
+    public void   setIdMedicament(int idMedicament)    { this.idMedicament = idMedicament; }
+    public String getNumeroLot()                       { return numeroLot; }
+    public void   setNumeroLot(String numeroLot)       { this.numeroLot = numeroLot; }
+    public int    getQuantite()                        { return quantite; }
+    public void   setQuantite(int quantite)            { this.quantite = quantite; }
+    public int    getSeuilAlerte()                     { return seuilAlerte; }
+    public void   setSeuilAlerte(int seuilAlerte)      { this.seuilAlerte = seuilAlerte; }
+    public double getPrixUnitaire()                    { return prixUnitaire; }
+    public void   setPrixUnitaire(double prixUnitaire) { this.prixUnitaire = prixUnitaire; }
+    public Date   getDateExpiration()                  { return dateExpiration; }
+    public void   setDateExpiration(Date d)            { this.dateExpiration = d; }
+    public Date   getDateReception()                   { return dateReception; }
+    public void   setDateReception(Date d)             { this.dateReception = d; }
+    public String getLocalisation()                    { return localisation; }
+    public void   setLocalisation(String localisation) { this.localisation = localisation; }
+    public String getFournisseur()                     { return fournisseur; }
+    public void   setFournisseur(String fournisseur)   { this.fournisseur = fournisseur; }
 
     @Override
     public String toString() {
         return "Stock{id=" + idStock + ", lot='" + numeroLot + "', quantite=" + quantite +
-               ", seuilAlerte=" + seuilAlerte + ", prix=" + prixUnitaire + "}";
+                ", seuilAlerte=" + seuilAlerte + ", prix=" + prixUnitaire + "}";
     }
 }
